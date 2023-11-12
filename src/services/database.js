5d6d37eb84d8e2eb007cd605de7a0678bc3b02f2
Ayush Gupta <ayushgupta3902@gmail.com>
2023-11-12 14:43:18 +0530

    feat: add database service

--- src/services/database.js
+++ src/services/database.js
@@ -1,5 +1,87 @@
 import conf from "../config/config";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
+export class DbService {
+  client = new Client();
+  databases;
+  bucket;
 
-export 
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.databases = new Databases(this.client);
+    this.storage = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, image, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug, // Here slug is used as unique Id, Id.unique() can also be used
+        {
+          title,
+          content,
+          image,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updatePost(slug, { title, content, image, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          image,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+        return await this.databases.getDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug
+        )
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+  }
+
+  
+}
+
+const dbService = new DbService();
+
+export default dbService;
