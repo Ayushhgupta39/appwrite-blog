498f617a6fcf01a8ad3f9400592b86e509d145fb
Ayush Gupta <ayushgupta3902@gmail.com>
2023-11-13 20:16:15 +0530

    feat: update database services

--- src/services/database.js
+++ src/services/database.js
@@ -68,18 +68,57 @@ export class DbService {
 
   async getPost(slug) {
     try {
-        return await this.databases.getDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug
-        )
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
     } catch (error) {
-        console.log(error);
-        return false;
+      console.log(error);
+      return false;
     }
   }
 
-  
+  async getAllPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries // Here we can straightaway write Query.equal("status", "active")]
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  //   Method for File upload
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.storage.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.storage.getFilePreview(conf.appwriteBucketId, fileId);
+  }
 }
 
 const dbService = new DbService();
