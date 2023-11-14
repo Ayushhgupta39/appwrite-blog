6d0cbc59419dcf6c5f8528e711247a903d34a754
Ayush Gupta <ayushgupta3902@gmail.com>
2023-11-14 18:52:10 +0530

    feat: setup tailwind and redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import './App.css'
-import conf from './config/config'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./services/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(conf);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />  
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
     <>
-      <h1>A blog app with appwrite</h1>
+      <p>Loading...</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
